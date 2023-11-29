ab6f070ab1733b35104eac6a0a1d8e7b5b5c9a65
MaximYushtein <147516231+MaximYushtein@users.noreply.github.com>
2023-11-29 15:54:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let speed  = 0
+let bmwspeed =0
+let bg=document.getElementById("bg")
+let bgspeed=0
 document.body.onkeydown=function(event){
   if(speed<500 && event.keyCode==32 ){
        speed=speed +5           
   }
-  console0.log(6974)  
-  let car =document.getElementById("car") 
-  car.style.left=speed+"px" 
-  
+  if(speed<500 && event.keyCode==38){
+bmwspeed=bmwspeed+3         
+}
+  console.log(event.keyCode)  
+//   let car =document.getElementById("car") 
+//   car.style.left=speed+"px" 
+//   console.log(6974)  
+//   let bmw =document.getElementById("bmw") 
+//   bmw.style.left=speed+"px" 
 }
 setInterval(function(){
-  
-    
   let car =document.getElementById("car") 
-  car.style.left=speed+"px" 
-},100)              
+  car.style.left=speed+"px"            
+let bmw =document.getElementById("bmw") 
+bmw.style.left=bmwspeed+"px" 
+},100)  
+setInterval(function(){
+bgspeed=bgspeed+1
+
+  },100) 
